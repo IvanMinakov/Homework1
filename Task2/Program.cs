39f9d9779d69588b513ed1efaa9d27ae3b9eb56a
Ivan Minakov <i.minakov786@gmail.com>
2022-11-22 06:02:20 -0500

    task 3 completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,11 +5,11 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
-if (b > max) 
+if (b > max)
 {
     max = b;
 }
-if (c > max) 
+if (c > max)
 {
     max = c;
 }
