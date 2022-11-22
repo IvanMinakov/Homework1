c808d461769589952e28b5ffabb82af16236d68c
Ivan Minakov <i.minakov786@gmail.com>
2022-11-22 07:43:05 -0500

    task 4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int K = 1;
+while (K <= N)
+{
+    if (K % 2 == 0)
+    {
+        Console.Write(K + " ,");
+    }
+    K +=1; 
+}
